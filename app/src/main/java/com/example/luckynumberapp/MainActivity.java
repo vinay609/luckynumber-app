a93799c4a285711d9f621a89b54ec563d470d10d
admin <22ismo07@uohyd.ac.in>
2024-12-24 11:52:25 +0530

    mod

--- app/src/main/java/com/example/luckynumberapp/MainActivity.java
+++ app/src/main/java/com/example/luckynumberapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.luckynumberapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+        EditText editText;
+        Button btn;
+        TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +27,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+        editText = findViewById(R.id.edit_text);
+        btn = findViewById(R.id.btn);
+        txt = findViewById(R.id.welcome);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editText.getText().toString();
+                Intent i = new Intent(
+                        getApplicationContext(),
+                        second_activitty.class
+                );
+                i.putExtra("name", username);
+                startActivity(i);
+                txt.setText("Welcome " + username);
+            }
+        });
+
     }
 }
